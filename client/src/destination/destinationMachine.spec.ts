e1904671427aee69ab8709d2aa2942413d04c308
kamil <frankiewiczkamil@gmail.com>
2023-02-06 00:04:10 +0100

    Refactor and improve destination path fetching (sub)machine

--- client/src/destination/destinationMachine.spec.ts
+++ client/src/destination/destinationMachine.spec.ts
@@ -1,22 +1,83 @@
-import { destinationMachine } from './destinationMachine';
+import { destinationMachine, EventDone, EventFailed } from './destinationMachine';
 import { assert, describe, test } from 'vitest';
 import { interpret } from 'xstate';
 
 describe('destinationMachine', () => {
-    test('transition IDLE -> IN PROGRESS', () => {
-        const actualState = destinationMachine.transition('idle', { type: 'request' });
-        assert(actualState.matches('in progress'));
+  test('should end with done state when service callback responds with a valid message', async () => {
+    const expectedPath = 'mocked path';
+    const machine = destinationMachine.withConfig({
+      services: {
+        fetchDestinationPath: async function () {
+          return { path: expectedPath };
+        },
+      },
     });
-    test('transition IN PROGRESS -> DONE', () => {
-        const machine = interpret(destinationMachine)
-            .onTransition((state) => {
-                if (state.matches('done')) {
-                    assert(true)
-                    // assert(state.event.path === 'mocked path');
-                    assert(state.context.destinationPath === 'mocked path');
-                }
-            });
-        machine.start();
-        machine.send('request');
+    const receivedPath = await new Promise((resolve) =>
+      interpret(machine)
+        .onTransition((state, event) => {
+          if (state.matches('done')) {
+            resolve((event as EventDone).data.path);
+          }
+        })
+        .start()
+    );
+    assert.equal(receivedPath, expectedPath);
+  });
+  test('should end with failed state when service callback breaks', async () => {
+    const expectedErrorMsg = 'zonk';
+    const machine = destinationMachine.withConfig({
+      services: {
+        fetchDestinationPath: async function () {
+          throw new Error(expectedErrorMsg);
+        },
+      },
     });
+    const receivedErrorMsg = await new Promise((resolve) =>
+      interpret(machine)
+        .onTransition((state, event) => {
+          if (state.matches('failed')) {
+            resolve((event as EventFailed).data.message);
+          }
+        })
+        .start()
+    );
+    assert.equal(receivedErrorMsg, expectedErrorMsg);
+  });
+
+  test('should retry fetching path on retry event in failed state', async () => {
+    const expectedPath = 'mocked path';
+    let attempt = 1;
+    const machine = destinationMachine.withConfig({
+      services: {
+        fetchDestinationPath: async function () {
+          if (attempt === 1) {
+            attempt++;
+            throw new Error('zonk');
+          } else {
+            return { path: expectedPath };
+          }
+        },
+      },
+    });
+    const service = interpret(machine);
+
+    await new Promise((resolve) => {
+      service
+        .onTransition((state, event) => {
+          if (state.matches('failed')) {
+            resolve(event.type);
+          }
+        })
+        .start();
+    });
+    const result = await new Promise((resolve) => {
+      service.onTransition((state, event) => {
+        if (state.matches('done')) {
+          resolve((event as EventDone).data.path);
+        }
+      });
+      service.send('RETRY_REQUESTED');
+    });
+    assert.equal(result, expectedPath);
+  });
 });
--- client/src/destination/destinationMachine.ts
+++ client/src/destination/destinationMachine.ts
@@ -1,57 +1,47 @@
-import { assign, createMachine } from "xstate";
+import { createMachine, DoneInvokeEvent } from 'xstate';
 
-type DestinationContext = {
-    error?: string;
-    destinationPath?: string;
+type Services = {
+  fetchDestinationPath: {
+    data: { path: string };
+  };
+};
+export type EventDone = { type: 'done.invoke.inProgressInvoke'; data: { path: string } };
+export type EventFailed = { type: 'error.platform.inProgressInvoke'; data: { message: string } };
+export type EventRetryRequested = { type: 'RETRY_REQUESTED' };
+type DestinationResponse = { path: string };
 
+function onDoneReturnDestinationPath(_ctx: {}, event: DoneInvokeEvent<DestinationResponse>): string {
+  return event.data.path;
 }
+
 export const destinationMachine =
-    /** @xstate-layout N4IgpgJg5mDOIC5QBE4BcCWA7Ahpg9lgAQAOeAFkQGZhoDG52UAdBhADZgDEATmAI4BXdEQjpseDIVIUA2gAYAuolAl8sDASwqQAD0QBaAIwB2AGzMAzCaPyAnNYCsAGhABPQ0bPzmADnnyJr5GAEyOAL7hrqiwmLhaMmiUNPSMWCzYpDz4UHywsFywgnR0cLAKykggahpaOvoIIQAsRsxGviYu7p7efgFBoRFRIDFxktJkSdS0DEysxCTZuWVcYDzZPBU6NZpS2lUNJibMTfJmYa4eCAYhlj525o7nQ9Hi8XuJyTNpLFQ4GJwILxaDw3Fsqjs6gdEEYmnZmPIjE8Lt1rrd7o9npFhlh8GJ4FVRhIEpMvqkmNt1LtCPVDJZLK1bA5OpdDKcLE0zJZHC0zHz+SZIq9YsSPqTpuT0vMsjk8gTVFSoaAGnYQm12SirsZev5AsEwkKRm9xgsKBLZlK2JxKbU9rS0b5LG17E5WdcvD5dQMDcMie8JmaUhbfv9ATbqftlTDQsw+SY7GYOl0tQzfMwE9zefy44a-SbPuafswIIQwOGlXpEJYOunEyzUdrPf19UNIkA */
-    createMachine<DestinationContext>({
-        id: 'Destination path fetching',
-        context: {},
-        schema: {
-            services: {} as {
-                fetchDestinationPath: {
-                    data: { destinationPath: string };
-                };
-            }
+  /** @xstate-layout N4IgpgJg5mDOIC5QBE4BcCWA7Ahpg9lgAQAOeAFkQGZhoDG52UAdBhADZgDEATmAI4BXdEQjpseDIVIUA2gAYAuolAl8sDASwqQAD0QBaAIwB2AGzMAzCaPyAnNYCsAGhABPQ0bPzmADnnyJr5GAEyOAL7hrqiwmLhaMmiUNPSMWCzYpDz4UHywsFywgnR0cLAKykggahpaOvoIIQAsRsxGviYu7p7efgFBoRFRIDFxktJkSdS0DEysxCTZuWVcYDzZPBU6NZpS2lUNJibMTfJmYa4eCAYhlj525o7nQ9Hi8XuJyTNpLFQ4GJwILxaDw3Fsqjs6gdEEYmnZmPIjE8Lt1rrd7o9npFhlh8GJ4FVRhIEpMvqkmNt1LtCPVDJZLK1bA5OpdDKcLE0zJZHC0zHz+SZIq9YsSPqTpuT0vMsjk8gTVFSoaAGnYQm12SirsZev5AsEwkKRm9xgsKBLZlK2JxKbU9rS0b5LG17E5WdcvD5dQMDcMie8JmaUhbfv9ATbqftlTDQsw+SY7GYOl0tQzfMwE9zefy44a-SbPuafswIIQwOGlXpEJYOunEyzUdrPf19UNIkA */
+  createMachine({
+    predictableActionArguments: true,
+    context: {},
+    tsTypes: {} as import('./destinationMachine.typegen').Typegen0,
+    schema: {
+      services: {} as Services,
+      events: {} as EventDone | EventFailed | EventRetryRequested,
+    },
+    initial: 'inProgress',
+    states: {
+      inProgress: {
+        invoke: {
+          id: 'inProgressInvoke',
+          src: 'fetchDestinationPath',
+          onDone: 'done',
+          onError: 'failed',
         },
-        initial: 'idle',
-        states: {
-            idle: {
-                on: {
-                    "request": "in progress"
-                }
-            },
-            ['in progress']: {
-                invoke: {
-                    src: 'fetchDestinationPath',
-                    onDone: {
-                        target: 'done',
-                        actions: assign({
-                            destinationPath: (context, event) => {
-                                const path = event?.data?.path;
-                                context.destinationPath = path;
-                                return path;
-                            }
-                        })
-                    },
-                    onError: 'failed'
-                },
-            },
-            failed: {
-                on: {
-                    retry: "in progress"
-                }
-            },
-            done: {}
-        }
-    }).withConfig({
-        services: {
-            fetchDestinationPath: async function () {
-                await Promise.resolve();
-                return { path: 'mocked path' };
-            }
-        }
-    });
+      },
+      failed: {
+        on: {
+          RETRY_REQUESTED: 'inProgress',
+        },
+      },
+      done: {
+        type: 'final',
+        data: onDoneReturnDestinationPath,
+      },
+    },
+  });
