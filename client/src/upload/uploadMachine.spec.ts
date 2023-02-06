3a82b4aa697d9a2e3c029fd86f4b74267e7de1ce
kamil <frankiewiczkamil@gmail.com>
2023-02-06 23:27:35 +0100

    Add file upload (sub)machine

---
+++ client/src/upload/uploadMachine.spec.ts
@@ -0,0 +1,79 @@
+import { EventDone, EventFailed, uploadMachine } from './uploadMachine';
+import { assert, describe, test } from 'vitest';
+import { interpret } from 'xstate';
+
+describe('uploadMachine', () => {
+  test('should end with done state when upload callback finishes successfully', async () => {
+    const machine = uploadMachine.withConfig({
+      services: {
+        uploadFile: async function () {
+          await Promise.resolve();
+        },
+      },
+    });
+
+    const state = await new Promise((resolve) =>
+      interpret(machine)
+        .onTransition((state, _event) => {
+          if (state.matches('done')) {
+            resolve(state.value);
+          }
+        })
+        .start()
+    );
+    assert.equal(state, 'done');
+  });
+  test('should end with failed state when upload callback breaks', async () => {
+    const expectedErrorMsg = 'zonk';
+    const machine = uploadMachine.withConfig({
+      services: {
+        uploadFile: async function () {
+          throw new Error(expectedErrorMsg);
+        },
+      },
+    });
+    const errorMsg = await new Promise((resolve) =>
+      interpret(machine)
+        .onTransition((state, event) => {
+          if (state.matches('failed')) {
+            resolve((event as EventFailed).data.message);
+          }
+        })
+        .start()
+    );
+    assert.equal(errorMsg, expectedErrorMsg);
+  });
+  test('should retry upload on retry event in failed state', async () => {
+    let attempt = 1;
+    const machine = uploadMachine.withConfig({
+      services: {
+        uploadFile: async function () {
+          if (attempt === 1) {
+            attempt++;
+            throw new Error('zonk');
+          }
+        },
+      },
+    });
+    const service = interpret(machine);
+
+    await new Promise((resolve) => {
+      service
+        .onTransition((state, event) => {
+          if (state.matches('failed')) {
+            resolve(event);
+          }
+        })
+        .start();
+    });
+    const event = await new Promise((resolve) => {
+      service.onTransition((state, event) => {
+        if (state.matches('done')) {
+          resolve(event);
+        }
+      });
+      service.send('RETRY_REQUESTED');
+    });
+    assert.equal((event as EventDone).type, 'done.invoke.inProgressInvokeUpload');
+  });
+});
---
+++ client/src/upload/uploadMachine.ts
@@ -0,0 +1,48 @@
+import { createMachine } from 'xstate';
+
+export type EventDone = { type: 'done.invoke.inProgressInvokeUpload' };
+export type EventFailed = { type: 'error.platform.inProgressInvokeUpload'; data: { message: string } };
+export type EventRetryRequested = { type: 'RETRY_REQUESTED' };
+type Event = EventDone | EventFailed | EventRetryRequested;
+
+type TypeState =
+  | {
+      value: 'done';
+      context: string;
+    }
+  | {
+      value: 'inProgress';
+      context: string;
+    }
+  | {
+      value: 'failed';
+      context: string;
+    };
+
+type Ctx = string;
+export const uploadMachine =
+  /** @xstate-layout N4IgpgJg5mDOIC5QFUAOAbA9gQwgAgDMBLdMAOiIlIGIAnMARwFc4AXAbQAYBdRUVTLCKsimAHZ8QAD0QBGACyyysgGzzOKgEwBWADQgAnnIDsATjLaAvpf1osuQiXJExeVLUxR6sWNQjjnMQA3TABrcjscfGJSCld3T29YBBcQgGNsEXEubhzJASEsiSRpRG1OYzJNFW1tU3l5AA5NeU1ZU30jBFkAZh7rWwwox1iXNw8vOF8wWg9aMgxMgkxaAFsySIcYwPHEqZTgzAyinLySguFRYtAZbsVlNQ0dTsRGpSsbEE3opzICbCcEDoYFYtAMZ34gku4kktwUSlU6i0ekMck0pmsnzEmAgcEk3xGYHyUKKsMQAFoVC8EJSBl8hltfpRSMTClcyQhtJoyCpHsjqbJGpw6QTtnFdpMfKzoddSggerJudoFZoevJjIo2px6gLOI0LCKGT9Yv9AdLSSVbq1qY0VGQ9X1HU7HYb7MbyP4xETziT2ZbECozGRTCpGsZOK1VSoeppGgKY5jLEA */
+  createMachine<Ctx, Event, TypeState>({
+    context: 'initial path',
+    predictableActionArguments: true,
+    // there's a problem with service definition when using typegen in this machine  ¯\_(ツ)_/¯
+    initial: 'inProgress',
+    states: {
+      inProgress: {
+        invoke: {
+          id: 'inProgressInvokeUpload',
+          src: 'uploadFile',
+          onDone: 'done',
+          onError: 'failed',
+        },
+      },
+      failed: {
+        on: {
+          RETRY_REQUESTED: 'inProgress',
+        },
+      },
+      done: {
+        type: 'final',
+      },
+    },
+  });
