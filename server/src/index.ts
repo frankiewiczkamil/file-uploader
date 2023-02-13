1e85b9874ee9ad3e11b70b49c9bf915cf923e173
kamil <frankiewiczkamil@gmail.com>
2023-02-13 20:57:00 +0100

    Add dummy server impl

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response } from 'express';
+import { v4 as uuid } from 'uuid';
+import cors from 'cors';
+
+const app: Express = express();
+app.use(cors());
+
+const port = 3000;
+
+app
+  .get('/path', (req: Request, res: Response) => {
+    res.send({ path: `http://localhost/upload/${uuid()}` });
+  })
+  .get('/upload/:id', (req: Request, res: Response) => {
+    res.send(req.params['id']);
+  });
+
+app.listen(port, () => {
+  console.log(`[server] start server http://localhost:${port}`);
+});
