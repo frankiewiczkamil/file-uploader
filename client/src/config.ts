3719dd615bcf81327a95aef95da50ff97eadf334
kamil <frankiewiczkamil@gmail.com>
2023-08-13 19:59:32 +0200

    add support for ssl backend endpoints

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,2 @@
-export const FETCH_DESTINATION_ENDPOINT = 'http://localhost:3000/path';
-export const NOTIFICATION_ENDPOINT = 'http://localhost:3000/notify';
+export const FETCH_DESTINATION_ENDPOINT = 'http://localhost:18080/path';
+export const NOTIFICATION_ENDPOINT = 'http://localhost:18080/notify';
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,21 +2,46 @@ import express, { Express, Request, Response } from 'express';
 import { v4 as uuid } from 'uuid';
 import cors from 'cors';
 import { uploadRoute } from './upload.js';
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 const app: Express = express();
 app.use(cors());
 
-const port = 3000;
+const port = {
+  default: 18080,
+  http1Ssl: 18443
+};
+
+const getUploadPath = (apiVersion) => {
+  if (apiVersion === '1') {
+    return `https://localhost:${port.http1Ssl}`;
+  } else {
+    return `http://localhost:${port.default}`;
+  }
+}
 
 app
   .get('/path', (req: Request, res: Response) => {
-    res.send({ path: `http://localhost:3000/upload/${uuid()}` });
+    res.send({ path: `${getUploadPath(req.params.apiVersion)}/upload/${uuid()}` });
   })
   .post('/upload/:id', uploadRoute)
   .post('/notify', (req: Request, res: Response) => {
     res.send({ message: 'roger roger' });
   });
 
-app.listen(port, () => {
-  console.log(`[server] start server http://localhost:${port}`);
-});
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+const certs = path.join(__dirname, '..', 'certs')
+
+const options = {
+  key: fs.readFileSync(`${certs}/key.pem`, 'utf8'),
+  cert: fs.readFileSync(`${certs}/cert.pem`, 'utf8')
+};
+http.createServer(app).listen(port.default)
+https.createServer(options, app).listen(port.http1Ssl)
