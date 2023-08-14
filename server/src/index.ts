6e85e0132dd521280b0977264e39dd470fdde8ef
kamil <frankiewiczkamil@gmail.com>
2023-08-14 21:43:23 +0200

    add support for http2 backend endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { uploadRoute } from './upload.js';
 import http from 'http';
 import https from 'https';
+import spdy from 'spdy'; // http2 module is not compatible with express v4
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -14,14 +15,16 @@ app.use(cors());
 
 const port = {
   default: 18080,
-  http1Ssl: 18443
+  http1Ssl: 18443,
+  http2Ssl: 28443,
+
 };
 
 const getUploadPath = (apiVersion) => {
   if (apiVersion === '1') {
     return `https://localhost:${port.http1Ssl}`;
   } else {
-    return `http://localhost:${port.default}`;
+    return `https://localhost:${port.http2Ssl}`;
   }
 }
 
@@ -37,11 +40,12 @@ app
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
-const certs = path.join(__dirname, '..', 'certs')
+const certs = path.join(__dirname, '..', 'certs');
 
 const options = {
   key: fs.readFileSync(`${certs}/key.pem`, 'utf8'),
   cert: fs.readFileSync(`${certs}/cert.pem`, 'utf8')
 };
-http.createServer(app).listen(port.default)
-https.createServer(options, app).listen(port.http1Ssl)
+http.createServer(app).listen(port.default);
+https.createServer(options, app).listen(port.http1Ssl);
+spdy.createServer(options, app).listen(port.http2Ssl);
